a85537806c3a6cadb6d9c7fe42f72345afd97c24
GuanRuLai <128710130+GuanRuLai@users.noreply.github.com>
2024-03-10 16:49:20 +0800

    Add files via upload

---
+++ form.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+# create flask app
+app = Flask(__name__)
+
+@app.route("/form")
+def formPage():
+    return render_template("Form.html")
+
+@app.route("/submit", methods = ["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        user = request.form["user"] # extract user data from form submission
+        print("post : user => ", user) # redirect to success route with user data
+        return redirect(url_for("success", name=user, action="post"))
+
+    else:
+        user = request.args.get("user") # extract user data from URL parameters
+        print("get : user => ", user)
+        return redirect(url_for("success", name=user, action="get")) # redirect to success route with user data
+
+@app.route("/success/<action>/<name>")
+def success(name, action):
+    return "{} : Welcome {} ~ !!".format(action, name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+#%%
